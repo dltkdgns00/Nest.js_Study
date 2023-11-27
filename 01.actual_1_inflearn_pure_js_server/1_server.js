d7f8ecd214ed6938fa69d76e055d2b20202a9683
SangHoon Lee <s20604@naver.com>
2023-11-27 21:08:37 +0900

    Path 별로 다른 응답 변환하기

--- 01.actual_1_inflearn_pure_js_server/1_server.js
+++ 01.actual_1_inflearn_pure_js_server/1_server.js
@@ -1,5 +1,7 @@
 // import http from 'http';
 const http = require('http');
+// import url from 'url';
+const url = require('url');
 
 // localhost -> 127.0.0.1 -> loop back -> 서버를 실행한 컴퓨터
 const host = 'localhost';
@@ -9,8 +11,28 @@ const port = 3000;
 // res -> response -> 응답
 const server = http.createServer((req, res) =>
 {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<h1>Hello World</h1>');
+    const path = url.parse(req.url).pathname;
+
+    if (path === '/')
+    {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('<h1>Home Page!</h1>');
+    }
+    else if (path === '/post')
+    {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('<h1>Post Page!</h1>');
+    }
+    else if (path === '/user')
+    {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('<h1>User Page!</h1>');
+    }
+    else
+    {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h1>404 Page Not Found!</h1>');
+    }
 });
 
 server.listen(port, host, () =>
