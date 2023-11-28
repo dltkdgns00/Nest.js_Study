2971b7b4fe813fd01e0a2dd5fac816e23ba38eda
SangHoon Lee <s20604@naver.com>
2023-11-28 17:06:40 +0900

    Service로 모든 로직 옮기기

--- 03.cf_sns/src/posts/posts.controller.ts
+++ 03.cf_sns/src/posts/posts.controller.ts
@@ -3,72 +3,11 @@ import {
   Controller,
   Delete,
   Get,
-  NotFoundException,
   Param,
   Patch,
   Post,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
-
-/**
- * author: string;
- * title: string;
- * content: string;
- * likeCount: number;
- * commentCount: number;
- */
-
-interface PostModel {
-  id: number;
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-let posts: PostModel[] = [
-  {
-    id: 1,
-    author: 'newjeans_official',
-    title: '뉴진스 민지',
-    content: '멤버들 때문에 버거운 민지',
-    likeCount: 10000000,
-    commentCount: 999999,
-  },
-  {
-    id: 2,
-    author: 'newjeans_official',
-    title: '뉴진스 하니',
-    content: '한국어 공부하는 하니',
-    likeCount: 10000000,
-    commentCount: 999999,
-  },
-  {
-    id: 3,
-    author: 'newjeans_official',
-    title: '뉴진스 다니엘',
-    content: '당근 먹고 있는 다니엘',
-    likeCount: 10000000,
-    commentCount: 999999,
-  },
-  {
-    id: 4,
-    author: 'newjeans_official',
-    title: '뉴진스 해린',
-    content: '이상한 해린',
-    likeCount: 10000000,
-    commentCount: 999999,
-  },
-  {
-    id: 5,
-    author: 'newjeans_official',
-    title: '뉴진스 혜인',
-    content: '장난 치고 있는 혜인',
-    likeCount: 10000000,
-    commentCount: 999999,
-  },
-];
 
 @Controller('posts')
 export class PostsController {
@@ -78,7 +17,7 @@ export class PostsController {
   //    모든 posts를 다 가져온다.
   @Get()
   getPosts() {
-    return posts;
+    return this.postsService.getAllPosts();
   }
 
   // 2) GET /posts/:id
@@ -86,12 +25,7 @@ export class PostsController {
   //    예를 들어서 id=1일 경우 id가 1인 post를 가져온다.
   @Get(':id')
   getPost(@Param('id') id: string) {
-    const post = posts.find((post) => post.id === +id);
-    if (!post) {
-      throw new NotFoundException();
-    }
-
-    return post;
+    return this.postsService.getPostById(+id);
   }
 
   // 3) POST /posts
@@ -102,18 +36,7 @@ export class PostsController {
     @Body('title') title: string,
     @Body('content') content: string,
   ) {
-    const post: PostModel = {
-      id: posts[posts.length - 1].id + 1,
-      author,
-      title,
-      content,
-      likeCount: 0,
-      commentCount: 0,
-    };
-
-    posts = [...posts, post];
-
-    return post;
+    return this.postsService.createPost(author, title, content);
   }
 
   // 4) PATCH /posts/:id
@@ -125,41 +48,13 @@ export class PostsController {
     @Body('title') title?: string,
     @Body('content') content?: string,
   ) {
-    const post = posts.find((post) => post.id === +id);
-
-    if (!post) {
-      throw new NotFoundException();
-    }
-
-    if (author) {
-      post.author = author;
-    }
-
-    if (title) {
-      post.title = title;
-    }
-
-    if (content) {
-      post.content = content;
-    }
-
-    posts = posts.map((prevPost) => (prevPost.id === +id ? post : prevPost));
-
-    return post;
+    return this.postsService.updatePost(+id, author, title, content);
   }
 
   // 5) DELETE /posts/:id
   //    id에 해당하는 post를 삭제한다.
   @Delete(':id')
   deletePost(@Param('id') id: string) {
-    const post = posts.find((post) => post.id === +id);
-
-    if (!post) {
-      throw new NotFoundException();
-    }
-
-    posts = posts.filter((post) => post.id !== +id);
-
-    return id;
+    return this.postsService.deletePost(+id);
   }
 }
--- 03.cf_sns/src/posts/posts.service.ts
+++ 03.cf_sns/src/posts/posts.service.ts
@@ -1,4 +1,132 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+/**
+ * author: string;
+ * title: string;
+ * content: string;
+ * likeCount: number;
+ * commentCount: number;
+ */
+
+export interface PostModel {
+  id: number;
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjeans_official',
+    title: '뉴진스 민지',
+    content: '멤버들 때문에 버거운 민지',
+    likeCount: 10000000,
+    commentCount: 999999,
+  },
+  {
+    id: 2,
+    author: 'newjeans_official',
+    title: '뉴진스 하니',
+    content: '한국어 공부하는 하니',
+    likeCount: 10000000,
+    commentCount: 999999,
+  },
+  {
+    id: 3,
+    author: 'newjeans_official',
+    title: '뉴진스 다니엘',
+    content: '당근 먹고 있는 다니엘',
+    likeCount: 10000000,
+    commentCount: 999999,
+  },
+  {
+    id: 4,
+    author: 'newjeans_official',
+    title: '뉴진스 해린',
+    content: '이상한 해린',
+    likeCount: 10000000,
+    commentCount: 999999,
+  },
+  {
+    id: 5,
+    author: 'newjeans_official',
+    title: '뉴진스 혜인',
+    content: '장난 치고 있는 혜인',
+    likeCount: 10000000,
+    commentCount: 999999,
+  },
+];
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  getAllPosts() {
+    return posts;
+  }
+
+  getPostById(id: number) {
+    const post = posts.find((post) => post.id === +id);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    return post;
+  }
+
+  createPost(author: string, title: string, content: string) {
+    const post: PostModel = {
+      id: posts[posts.length - 1].id + 1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    };
+
+    posts = [...posts, post];
+
+    return post;
+  }
+
+  updatePost(
+    postId: number,
+    author?: string,
+    title?: string,
+    content?: string,
+  ) {
+    const post = posts.find((post) => post.id === +postId);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    if (author) {
+      post.author = author;
+    }
+
+    if (title) {
+      post.title = title;
+    }
+
+    if (content) {
+      post.content = content;
+    }
+
+    return post;
+  }
+
+  deletePost(postId: number) {
+    const post = posts.find((post) => post.id === postId);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    posts = posts.filter((post) => post.id !== postId);
+
+    return postId;
+  }
+}
