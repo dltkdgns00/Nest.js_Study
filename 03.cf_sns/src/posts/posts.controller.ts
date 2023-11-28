37653a3eb7c0a1c3da2eaece6a18347becff520b
SangHoon Lee <s20604@naver.com>
2023-11-28 16:22:14 +0900

    Patch Post 엔드포인트 생성하기

--- 03.cf_sns/src/posts/posts.controller.ts
+++ 03.cf_sns/src/posts/posts.controller.ts
@@ -4,6 +4,7 @@ import {
   Get,
   NotFoundException,
   Param,
+  Patch,
   Post,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
@@ -25,7 +26,7 @@ interface PostModel {
   commentCount: number;
 }
 
-const posts: PostModel[] = [
+let posts: PostModel[] = [
   {
     id: 1,
     author: 'newjeans_official',
@@ -87,7 +88,9 @@ export class PostsController {
     const post = posts.find((post) => post.id === +id);
     if (!post) {
       throw new NotFoundException();
-    } else return post;
+    }
+
+    return post;
   }
 
   // 3) POST /posts
@@ -107,13 +110,42 @@ export class PostsController {
       commentCount: 0,
     };
 
-    posts.push(post);
+    posts = [...posts, post];
 
     return post;
   }
 
-  // 4) PUT /posts/:id
+  // 4) PATCH /posts/:id
   //    id에 해당하는 post를 수정한다.
+  @Patch(':id')
+  patchPost(
+    @Param('id') id: string,
+    @Body('author') author?: string,
+    @Body('title') title?: string,
+    @Body('content') content?: string,
+  ) {
+    const post = posts.find((post) => post.id === +id);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    if (author) {
+      post.author = author;
+    }
+
+    if (title) {
+      post.title = title;
+    }
+
+    if (content) {
+      post.content = content;
+    }
+
+    posts = posts.map((prevPost) => (prevPost.id === +id ? post : prevPost));
+
+    return post;
+  }
 
   // 5) DELETE /posts/:id
   //    id에 해당하는 post를 삭제한다.
