a79fdf6b16ff02c49b88d726478e76b075ad65cb
SangHoon Lee <s20604@naver.com>
2023-11-30 16:11:12 +0900

    Entity로 테이블 생성하기

--- 03.cf_sns/src/app.module.ts
+++ 03.cf_sns/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostsModel } from './posts/entities/posts.entity';
 
 @Module({
   imports: [
@@ -15,7 +16,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'postgres',
       password: 'postgres',
       database: 'postgres',
-      entities: [],
+      entities: [PostsModel],
       synchronize: true,
     }),
   ],
