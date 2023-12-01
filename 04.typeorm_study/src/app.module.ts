30560525fa9e57d6a0388a3c5408b057310ec3f1
SangHoon Lee <s20604@naver.com>
2023-12-01 19:58:37 +0900

    Entity Embedding

--- 04.typeorm_study/src/app.module.ts
+++ 04.typeorm_study/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModel } from './entity/user.entity';
+import { StudentModel, TeacherModel } from './entity/person.entity';
 
 @Module({
   imports: [
@@ -14,7 +15,7 @@ import { UserModel } from './entity/user.entity';
       username: 'postgres',
       password: 'postgres',
       database: 'typeormstudy',
-      entities: [UserModel],
+      entities: [UserModel, StudentModel, TeacherModel],
       synchronize: true,
     }),
   ],
