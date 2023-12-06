0eb2d831e11d978782add53b25d1b9d8a2b9c04b
SangHoon Lee <s20604@naver.com>
2023-12-06 18:42:51 +0900

    Typeorm 유틸리티 탐구

--- 04.typeorm_study/src/app.controller.ts
+++ 04.typeorm_study/src/app.controller.ts
@@ -1,7 +1,17 @@
 import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserModel } from './entity/user.entity';
-import { Repository } from 'typeorm';
+import {
+  ILike,
+  IsNull,
+  LessThan,
+  LessThanOrEqual,
+  Like,
+  MoreThan,
+  MoreThanOrEqual,
+  Not,
+  Repository,
+} from 'typeorm';
 import { ProfileModel } from './entity/profile.entity';
 import { PostModel } from './entity/post.entity';
 import { TagModel } from './entity/tag.entity';
@@ -20,28 +30,54 @@ export class AppController {
   ) {}
 
   @Post('users')
-  postUser() {
-    return this.userRepository.save({
-      email: '1234@gmail.com',
-    });
+  async postUser() {
+    for (let i = 0; i < 100; i++) {
+      await this.userRepository.save({
+        email: `user-${i}@google.com`,
+      });
+    }
   }
 
   @Get('users')
   getUsers() {
     return this.userRepository.find({
+      where: {
+        // 아닌경우 가져오기
+        // id: Not(1),
+        // 작은경우 가져오기
+        // id: LessThan(30),
+        // 작거나 같은 경우
+        // id: LessThanOrEqual(30),
+        // 큰경우
+        // id: MoreThan(30),
+        // 크거나 같은 경우
+        // id: MoreThanOrEqual(30),
+        // 같은 경우
+        // id: Equal(30),
+        // 유사값
+        // email: Like('%google%'),
+        // 대문자 소문자 구분 안하는 유사값
+        // email: ILike('%GOOGLE%'),
+        // 사이값
+        // id: Between(10, 15),
+        // 해당되는 여러개의 값
+        // id: In([1, 3, 5, 7, 99]),
+        id: IsNull(),
+      },
+
       // 어떤 프로퍼티를 선택할지
       // 기본은 모든 프로퍼티를 가져온다
       // 만약에 select를 정의하지 않으면
       // select를 정의하면 정의된 프로퍼티들만 가져오게된다.
-      select: {
-        id: true,
-        createdAt: true,
-        updatedAt: true,
-        version: true,
-        profile: {
-          id: true,
-        },
-      },
+      // select: {
+      //   id: true,
+      //   createdAt: true,
+      //   updatedAt: true,
+      //   version: true,
+      //   profile: {
+      //     id: true,
+      //   },
+      // },
       // 필터링할 조건을 입력하게된다.
       // {} -> AND
       // [] -> OR
@@ -51,19 +87,19 @@ export class AppController {
       //   },
       // },
       // 관계를 가져오는 법
-      relations: {
-        profile: true,
-      },
+      // relations: {
+      //   profile: true,
+      // },
       // 오름차 내림차
       // ASC -> 오름차
       // DESC -> 내림차
-      order: {
-        id: 'DESC',
-      },
+      // order: {
+      //   id: 'DESC',
+      // },
       // 처음 몇개를 제외할지
-      skip: 0,
+      // skip: 0,
       // 몇개를 가져올지
-      take: 1,
+      // take: 1,
     });
   }
 
