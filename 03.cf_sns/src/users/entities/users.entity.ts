3ee7678880165c1064d8e9af0b995e47171397f4
SangHoon Lee <s20604@naver.com>
2023-12-06 19:33:28 +0900

    Column 옵션 탐구

--- 03.cf_sns/src/users/entities/users.entity.ts
+++ 03.cf_sns/src/users/entities/users.entity.ts
@@ -22,10 +22,20 @@ export class UsersModel {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({
+    // 1)
+    length: 20,
+    // 2)
+    unique: true,
+  })
+  // 1) 길이가 20을 넘지 않을 것
+  // 2) 유일무이한 값이 될 것
   nickname: string;
 
-  @Column()
+  @Column({
+    unique: true,
+  })
+  // 1) 유일무이한 값이 될 것
   email: string;
 
   @Column()
