63c44c8cb147cfce4edbf3611bc4d36cb81e4b48
SangHoon Lee <s20604@naver.com>
2024-01-05 15:54:42 +0900

    Token Refresh 기능 정리하기

--- 03.cf_sns/src/auth/auth.service.ts
+++ 03.cf_sns/src/auth/auth.service.ts
@@ -13,6 +13,26 @@ export class AuthService {
   ) {}
 
   /**
+   * 토큰을 사용하게 되는 방식
+   *
+   * 1) 사용자가 로그인 또는 회원가입을 진행하면
+   *    accessToken과 refreshToken을 반환한다.
+   * 2) 로그인 할때는 Basic 토큰과 함께 요청을 보낸다.
+   *    Basic 토큰은 '이메일:비밀번호'를 Base64로 인코딩한 형태이다.
+   *    예) {authorization: 'Basic {token}'}
+   * 3) 아무나 접근 할 수 없는 정보 {private route}를 접근 할때는
+   *    accessToken을 Header에 추가해서 요청과 함께 보낸다.
+   *    예) {authorization: 'Bearer {token}'}
+   * 4) 토큰과 요청을 함께 받은 서버는 토큰 검증을 통해 현재 요청을 보낸 사용자가 누구인지 알 수 있다.
+   *    예) 현재 로그인한 사용자가 작성한 포스트만 가져오려면 토큰의 sub 값에 입력돼있는 사용자의 포스트만 따로 필터링 할 수 있다.
+   *        특정 사용자의 토큰이 없다면 다른 사용자의 데이터를 접근 못한다.
+   * 5) 모든 토큰은 만료 기간이 있다. 만료 기간이 지나면 새로 토큰을 발급 받아야 한다.
+   *    그렇지 않으면 jwtService.verify()에서 검증이 실패한다.
+   *    그러니 accessToken을 새로 발급 받을 수 있는 /auth/token/access와 refreshToken을 새로 발급 받을 수 있는 /auth/token/refresh가 필요하다.
+   * 6) 토큰이 만료되면 각각의 토큰을 새로 발급 받을 수 있는 엔트포인트에 요청을 해서 새로운 토큰을 발급받고 새로운 토큰을 사용해서 private route에 접근한다.
+   */
+
+  /**
    * 1) registerWithEmail
    *  - email, nickname, password를 입력받고 사용자를 생성한다.
    *  - 생성이 완료되면 accessToken과 refreshToken을 반환한다.
--- 03.cf_sns/src/users/users.controller.ts
+++ 03.cf_sns/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
