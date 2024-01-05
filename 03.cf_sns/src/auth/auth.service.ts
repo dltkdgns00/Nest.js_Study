395dd0b147fe8ef7ffc031cc266e1b3484de81f0
SangHoon Lee <s20604@naver.com>
2024-01-05 16:12:29 +0900

    헤더 값으로부터 토큰 추출하는 로직 작성하기

--- 03.cf_sns/src/auth/auth.service.ts
+++ 03.cf_sns/src/auth/auth.service.ts
@@ -33,6 +33,26 @@ export class AuthService {
    */
 
   /**
+   * Header로 부터 토큰을 받을 떄
+   *
+   * {authorization: 'Basic {token}'}
+   * {authorization: 'Bearer {token}'}
+   */
+  async extractTokenFromHeader(header: string, isBearer: boolean) {
+    const splitToken = header.split(' ');
+
+    const prefix = isBearer ? 'Bearer' : 'Basic';
+
+    if (splitToken.length !== 2 || splitToken[0] !== prefix) {
+      throw new UnauthorizedException('잘못된 토큰입니다.');
+    }
+
+    const token = splitToken[1];
+
+    return token;
+  }
+
+  /**
    * 1) registerWithEmail
    *  - email, nickname, password를 입력받고 사용자를 생성한다.
    *  - 생성이 완료되면 accessToken과 refreshToken을 반환한다.
